0928b3e6d407e5147ee611301c37da9384e58d67
Alan Oliveira <alla.ao4975@gmail.com>
2021-09-05 15:43:10 -0300

    Dicionário Nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -8,15 +8,17 @@
 export default {
   data(){
     return{
-      data: undefined
+      data: []
     }
   },
   methods:{
     async pesquisar(e){
       try {
         const response = await this.$axios.$get(`/meanings/${e}`);
-        this.data = response
+        console.log(response.status)
+        this.data = response;
       } catch (error) {
+        this.data = null
           console.error(error)
       }
     }
@@ -29,19 +31,6 @@ export default {
   height: 100vh;
   padding: 2rem 1rem;
   position: relative;
-
-  // &::before{
-  //   content: '';
-  //   position: absolute;
-  //   top: 0;
-  //   left: 0;
-  //   width: 100%;
-  //   height: 100%;
-  //   background: linear-gradient(#000, #fff);
-  //   clip-path: circle(30% at right 70%);
-  //   opacity: .1;
-    
-  // }
 }
 
 </style>
